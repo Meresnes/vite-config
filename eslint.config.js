7666c971199e8af32ff0484f6e0483a07eb7b390
Meresnes <meresnes@yandex.ru>
2025-05-11 11:44:01 +0300

    fix config

--- eslint.config.js
+++ eslint.config.js
@@ -6,45 +6,44 @@ import stylisticTs from '@stylistic/eslint-plugin-ts'
 import importPlugin from 'eslint-plugin-import'
 import tseslint from 'typescript-eslint'
 
-export default tseslint.config(
-    { ignores: ['dist'] },
+export default tseslint.config({ ignores: ['dist'] },
     {
-      extends: [js.configs.recommended, ...tseslint.configs.recommended],
-      files: ['**/*.{ts,tsx}'],
-      languageOptions: {
-        ecmaVersion: 2020,
-        globals: globals.browser,
-      },
-      plugins: {
-        'react-hooks': reactHooks,
-        'react-refresh': reactRefresh,
-        '@stylistic/ts': stylisticTs,
-        'import': importPlugin,
-      },
-      node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx', '.d.ts', '.json', '.svg'],
-      },
-      settings: {
-        'import/resolver': {
-          typescript: { project: './tsconfig.json' },
+        extends: [js.configs.recommended, ...tseslint.configs.recommended],
+        files: ['**/*.{ts,tsx}'],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
         },
-      },
-      rules: {
-        ...reactHooks.configs.recommended.rules,
-        'react-refresh/only-export-components': [
-          'warn',
-          { allowConstantExport: true },
-        ],
-        '@stylistic/ts/indent': ['error', 4],
-        '@stylistic/ts/quotes': ['error', 'double', { avoidEscape: true }],
-        '@stylistic/ts/semi': ['error', 'always'],
-        'import/order': ['error', {
-          groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
-          alphabetize: { order: 'asc', caseInsensitive: true },
-        }],
-        'import/no-unresolved': 'error',
-        'import/newline-after-import': 'error',
-        'import/no-duplicates': 'error',
-      },
+        plugins: {
+            'react-hooks': reactHooks,
+            'react-refresh': reactRefresh,
+            '@stylistic/ts': stylisticTs,
+            'import': importPlugin,
+        },
+        settings: {
+            'import/resolver': {
+                typescript: { project: './tsconfig.json' },
+            },
+            node: {
+                extensions: ['.js', '.jsx', '.ts', '.tsx', '.d.ts', '.json', '.svg'],
+            },
+        },
+        rules: {
+            ...reactHooks.configs.recommended.rules,
+            'react-refresh/only-export-components': [
+                'warn',
+                { allowConstantExport: true },
+            ],
+            '@stylistic/ts/indent': ['error', 4],
+            '@stylistic/ts/quotes': ['error', 'double', { avoidEscape: true }],
+            '@stylistic/ts/semi': ['error', 'always'],
+            'import/order': ['error', {
+                groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+                alphabetize: { order: 'asc', caseInsensitive: true },
+            }],
+            'import/no-unresolved': 'warn',
+            'import/newline-after-import': 'error',
+            'import/no-duplicates': 'error',
+        },
     },
 )
